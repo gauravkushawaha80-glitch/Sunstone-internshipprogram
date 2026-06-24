ef03c04571cc0027822eb590a897b995d47d21ab
gauravkushawaha80-glitch <gauravkushawaha80@gmail.com>
2026-06-24 14:47:34 +0530

    notes

---
+++ lecturn-14/DOM1.js
@@ -0,0 +1,37 @@
+
+        // DOM Manipulation
+        function showMessage() {
+            let name = document.getElementById("name").value;
+
+            if(name === ""){
+                alert("Please enter your name");
+            } else {
+                document.getElementById("output").innerHTML =
+                "Welcome " + name + "!";
+            }
+        }
+
+        // DOM + Random Color
+        function changeColor() {
+            let colors = ["lightblue","lightgreen","pink","yellow","orange"];
+            let random = Math.floor(Math.random() * colors.length);
+            document.body.style.backgroundColor = colors[random];
+        }
+
+        // BOM - Date and Time
+        function showTime() {
+            document.getElementById("output").innerHTML =
+            "Current Time: " + new Date().toLocaleTimeString();
+        }
+
+        // BOM - window object
+        function openGoogle() {
+            window.open("https://www.google.com");
+        }
+
+        // BOM - setInterval (updates title every second)
+        setInterval(() => {
+            document.title = new Date().toLocaleTimeString();
+        }, 1000);
+
+ 
---
+++ lecturn-14/Dom.js
@@ -0,0 +1,8 @@
+let heading=document.getElementById("h");
+let p =document.getElementById("para");
+console.log(heading);
+console.log(p);
+let h=document.createElement("h2");
+h2.innerText="helllo";
+h2.style.color="blue";
+document.body.appendChild(h);
